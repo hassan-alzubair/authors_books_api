ab9c912ae8465446631c944ea2f5af500ae61982
Hassan Alzlubair <cs.hassan@hotmail.com>
2020-03-16 15:44:02 +0200

    add validation to book model

--- src/models/book.js
+++ src/models/book.js
@@ -1,10 +1,45 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
     const book = sequelize.define('book', {
-        title: DataTypes.STRING,
-        isbn: DataTypes.STRING,
-        author_id: DataTypes.INTEGER,
-        pages_count: DataTypes.INTEGER
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: true,
+                len: {
+                    args: [5],
+                    msg: 'title length must be between 5 an 10'
+                },
+                is: {
+                    args: /^[a-zA-Z0-9\s\.]*$/,
+                    msg: 'please provide a valid title (letters white spaces and numbers only)'
+                }
+            }
+        },
+        isbn: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                is: {
+                    args: /^ISBN\-\d{3}\-\d{4}$/i,
+                    msg: 'please provide a valid ISBN, should be like: ISBN-123-1234'
+                }
+            }
+        },
+        author_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isInt: true
+            }
+        },
+        pages_count: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isInt: true
+            }
+        }
     }, {
         underscored: true,
         updatedAt: 'updated_at',
